c035204afde73aaafb9bbb09a0b71bcc813c2efd
23038152 <23038152@student.hhs.nl>
2024-11-20 15:03:04 +0100

    schermen gekoppeld

--- src/main/java/com/example/ja/LoginController.java
+++ src/main/java/com/example/ja/LoginController.java
@@ -2,8 +2,14 @@ package com.example.ja;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -18,6 +24,18 @@ public class LoginController {
 
         if (username.equals("admin") && password.equals("admin")) {
             System.out.println("Login successful!");
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("HomePage.fxml"));
+                Parent root = loader.load();
+
+                HomePageController controller = loader.getController();
+
+                Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         } else {
             System.out.println("Login failed!");
         }
