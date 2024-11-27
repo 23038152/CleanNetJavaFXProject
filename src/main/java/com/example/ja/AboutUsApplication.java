763c5c33e4ba81b0bbab5cb23bc484800d6e3393
TihoSE <raymondip57@gmail.com>
2024-11-27 11:45:41 +0100

    2.0

--- src/main/java/com/example/ja/AboutUsApplication.java
+++ src/main/java/com/example/ja/AboutUsApplication.java
@@ -1,32 +1,23 @@
+package com.example.ja;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainApp.extends Application {
+import java.io.IOException;
 
+public class AboutUsApplication extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Laad het aboutUs.fxml-bestand
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/aboutUs.fxml"));
-            Parent root = loader.load();
-
-            // Maak een scène met het geladen FXML-bestand
-            Scene scene = new Scene(root, 800, 600); // Pas breedte en hoogte aan indien nodig
-
-            // Stel het venster in
-            primaryStage.setTitle("About Us Page");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace(); // Toon de fout in de console als er iets misgaat
-        }
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/ja/AboutUS.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("AboutUs");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args); // Start de JavaFX-toepassing
+        launch();
     }
 }
