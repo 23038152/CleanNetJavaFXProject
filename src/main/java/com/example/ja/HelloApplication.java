caf746ad163c68366facfaaaa8f8ece1a80c89a2
23038152 <23038152@student.hhs.nl>
2024-12-11 11:06:50 +0100

    .

--- src/main/java/com/example/ja/HelloApplication.java
+++ src/main/java/com/example/ja/HelloApplication.java
@@ -4,14 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginScherm.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Login scherm");
         stage.setScene(scene);
         stage.show();
