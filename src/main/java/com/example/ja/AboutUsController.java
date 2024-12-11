8328cda98b04d080d7a410eb8e28ea21d65cba8b
Robuit <24012645@student.hhs.nl>
2024-12-11 10:58:31 +0100

    Logo overal toegevoegd en layout verbeterd

--- src/main/java/com/example/ja/AboutUsController.java
+++ src/main/java/com/example/ja/AboutUsController.java
@@ -1,14 +1,42 @@
 package com.example.ja;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AboutUsController {
+
+
+
+    // Methode voor de System Notifs knop
+    @FXML
+    public void handleButtonClickNotifs(MouseEvent event) {
+        try {
+            // Laad de Notificatiepagina FXML
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/ja/MaintenancePage.fxml"));
+            Scene notifsScene = new Scene(fxmlLoader.load());
+
+            // Haal het huidige venster op en wijzig de scene
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(notifsScene);
+            stage.setTitle("Maintenance Page");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     private Label infoLabel;
 
     @FXML
-    protected void onAboutUsd() {
+    protected void onAboutUs() {
         infoLabel.setText("Welkom bij de About Us pagina!");
     }
 }
